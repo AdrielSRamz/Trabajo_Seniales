bcb19f4d09be00ba8fca1062e11f3ff2463829e6
AdrielSRamz <adrielsilva@live.com.ar>
2021-04-23 14:00:21 -0300

    Conexion manual Menu con Generators
    Como dije en la anterior descripción si habia un error en el label y no se actualizaba porque estaba en el ready. Se corrigio. Pero ahora surge la duda de si cada vez que se llama a la función se esta creando un label, pareciera que no, que solo se actualiza.

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,15 +1,11 @@
 extends TextureButton
 var generatorred = preload("res://scenes/Blocks/Red/BlockRed.tscn")
-
 	
 	
 	
-
-
 func _on_RedGenerator_button_down():
 	generate_block()
 	pass # Replace with function body.
-	
 func generate_block():
 	var red_block = generatorred.instance()
 	add_child(red_block)
--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -6,11 +6,14 @@ var instancesCount = 0
 var generatorred = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 var generatorviolet = preload("res://scenes/Buttons/VioletGenerator/VioletGenerator.tscn")
 func _ready():
+	$RedGenerator.connect("button_down", self, "count_new_instance")
+	$VioletGenerator.connect("button_down", self, "count_new_instance")
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
 func count_new_instance():
 	instancesCount += 1
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 
 	pass 
